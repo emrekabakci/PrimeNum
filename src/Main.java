fced5bc4a14cbc017d981c0a52a6d9e1fe241ccb
emrekabakci <emrekabakci.2@icloud.com>
2023-12-16 11:11:43 +0300

    Comment added.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
+        //Variables defined.
         int num = 100;
         int counter=0;
 
+        //Prime number founded and printed every time between 0 - 100
         for (int i = 1; i < num; i++) {
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
